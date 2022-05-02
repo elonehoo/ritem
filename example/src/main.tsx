b3f3d7d0f338d7b4a5236f762092f71239a82378
xiaoxunyao <huchengyea@163.com>
2022-05-02 16:11:14 +0800

    fix: update core

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Main from '@ritem/core'
+import App from './pages/index'
 import Footer from '@ritem/footer'
 
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 
+function go(){
+  console.log('go')
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <main className="font-sans px-4 py-10 text-center gray-700 dark:gray-200">
-      <Main name="Elone Hoo" />
-      <Footer githubLink='elonehoo'/>
+      <App />
+      <Footer githubLink='https://github.com/elonehoo/ritem'/>
     </main>
   </React.StrictMode>,
 )
--- packages/core/src/main.tsx
+++ packages/core/src/main.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React,{DetailedHTMLProps,ButtonHTMLAttributes} from 'react'
 
 class Props{
   name:string
+  go:any
+  nameModel:any
 }
 
 function Main(props:Props) {
@@ -22,12 +24,13 @@ function Main(props:Props) {
         <input
           placeholder="What's your name?"
           value={props.name}
+          onChange={props.nameModel}
           className="px-4 py-2 w-64 text-center bg-transparent outline-none border rounded border-solid border-gray-200 dark:border-gray-700 active:outline-none"
           autoComplete="false"
           type="text" />
 
         <div>
-            <button disabled={props.name.length <= 0} className="m-3 text-sm btn">
+            <button onClick={props.go} disabled={props.name.length <= 0} className="m-3 text-sm btn">
               Go
             </button>
         </div>
